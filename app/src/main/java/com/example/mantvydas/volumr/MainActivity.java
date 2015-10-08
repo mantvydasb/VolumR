a5fac77cc101f2f9128a75fe16be44d9c7e19cb6
mantvydas <mantvydo@gmail.com>
2015-10-07 21:44:55 -0700

    housekeeping

--- app/src/main/java/com/example/mantvydas/volumr/MainActivity.java
+++ app/src/main/java/com/example/mantvydas/volumr/MainActivity.java
@@ -2,12 +2,10 @@ package com.example.mantvydas.volumr;
 
 import android.animation.ObjectAnimator;
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,18 +20,23 @@ public class MainActivity extends AppCompatActivity {
     private ObjectAnimator objectAnimator = new ObjectAnimator();
     float scaleStart = 0.3f, scaleFinish = 1, scaleGone = 0;
     private TextView volumeLevel;
+    Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/tungsten_light.otf");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/tungsten_light.otf");
         volumeController = (ImageButton) findViewById(R.id.volume_controller);
-        volumeLevel = (TextView) findViewById(R.id.volumeLevel);
-        volumeLevel.setTypeface(typeface);
         collapseVolumeController();
 
+        volumeLevel = (TextView) findViewById(R.id.volumeLevel);
+        volumeLevel.setTypeface(typeface);
+
+        setVolumeDragHandler();
+    }
+
+    private void setVolumeDragHandler() {
         dragHandler = new DragHandler(volumeController, this, new DragHandler.OnDragListener() {
             @Override
             public void onYChanged(float y) {
@@ -49,10 +52,6 @@ public class MainActivity extends AppCompatActivity {
             public void onOneFingerDown() {
                 objectAnimator.ofFloat(volumeController, "scaleX", scaleStart, scaleFinish).start();
                 objectAnimator.ofFloat(volumeController, "scaleY", scaleStart, scaleFinish).start();
-//
-//                ObjectAnimator objectAnimator1 = new ObjectAnimator();
-//                objectAnimator.ofFloat(volumeController, "rotate", 0, 360).start();
-//                objectAnimator1.start();
             }
         });
 
@@ -76,7 +75,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -85,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
