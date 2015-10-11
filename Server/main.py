c4e4e0a1b07c1a049014740953cb36597be0a1e0
mantvydas <mantvydo@gmail.com>
2015-10-10 18:26:35 -0700

    started implementing ipv4 parser

--- Server/main.py
+++ Server/main.py
@@ -3,13 +3,31 @@ import os
 import volumr
 import subprocess
 
-
-output = subprocess.check_output("ipconfig",  shell=True)
-print(output)
-res = output.index("Ethernet adapter Ethernet:".encode("utf8"))
-print(res)
+START_FROM = 0
+IPv4_INDEXES = []
 
 
-volumrServer = volumr.Server()
+def getLocalIp():
+    output = getIpconfigOutput()
+    print(output)
+    findipv4Index(output, START_FROM)
+
+
+def findipv4Index(output, startFrom):
+    index = output.find("IPv4 Address".encode("utf8"), startFrom)
+
+    if index > 0:
+        print(index)
+        IPv4_INDEXES.append(index)
+        findipv4Index(output, index + 10)
+
+
+def getIpconfigOutput():
+    return subprocess.check_output("ipconfig /all", shell=True)
+
+
+getLocalIp()
+
+# volumrServer = volumr.Server()
 
 
