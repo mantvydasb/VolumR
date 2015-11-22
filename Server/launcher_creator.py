b2e964de5b6daec29cb9fc8d8980d474f88c9c5f
mantvydas <mantvydo@gmail.com>
2015-11-22 16:32:38 +0000

    updated scrip to reflect app name

--- Server/launcher_creator.py
+++ Server/launcher_creator.py
@@ -1,6 +1,7 @@
 import config
 
-VOLUMR_PATH = config.PATH_HOME_DIR + "\\volumr.exe"
+APP_NAME = config.APP_NAME
+VOLUMR_PATH = config.PATH_HOME_DIR + "\\" + APP_NAME + ".exe"
 SCRIPT_BODY = \
     "Set WshShell = CreateObject(" + '"WScript.Shell"'") \n" \
     "WshShell.Run" + ' "' + VOLUMR_PATH + '"' + ", 0 \n" \
@@ -12,7 +13,7 @@ def createScript():
     Generates a VBS script that will launch volumr.exe in a silent mode (no UI will be shown) everytime the user boots up the machine.
     :return:
     """
-    launcherFile = open("volumr_silent.vbs", mode="w+");
+    launcherFile = open(APP_NAME + "_silent.vbs", mode="w+");
     launcherFile.write(SCRIPT_BODY)
     launcherFile.close()
 
