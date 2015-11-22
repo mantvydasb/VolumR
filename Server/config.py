302c4d463df983ad7e6416ba03390b8d507d06f5
mantvydas <mantvydo@gmail.com>
2015-11-22 16:41:13 +0000

    renamed vars

--- Server/config.py
+++ Server/config.py
@@ -1,5 +1,5 @@
 import os.path
 
 APP_NAME = "volumr"
-APP_LAUNCHER = APP_NAME + "_silent.vbs"
+APP_SILENT_LAUNCHER = APP_NAME + "_silent.vbs"
 PATH_HOME_DIR = os.path.dirname(os.path.realpath(__file__))
