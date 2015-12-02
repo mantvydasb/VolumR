7f62c47e8d73816ac305792c0c56eba54cfb5d74
mantvydas <mantvydo@gmail.com>
2015-12-02 23:31:37 +0000

    implemented new commands for changing volume

--- Server/client.py
+++ Server/client.py
@@ -22,6 +22,22 @@ def moveRight():
     win32api.keybd_event(win32con.VK_RIGHT, 0, 0, 0)
     threading.Timer(1.0, moveRight).start()
 
-moveRight()
+# moveRight()
 
-
+# import socket
+# import sys
+# import struct
+#
+# address = ('localhost', 6005)
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# client_socket.connect(address)
+#
+# messages = ["foobar", "barbaz", "bazquxfad", "Jimmy Carter"]
+#
+# for s in messages:
+#
+#     totallen = len(s)
+#     pack1 = struct.pack('>I', totallen) # the first part of the message is length
+#     client_socket.sendall(pack1)
+#     client_socket.sendall(s.encode('utf8'))
+#
--- Server/server.py
+++ Server/server.py
@@ -7,12 +7,14 @@ MAX_VOLUME = 65535
 VOLUME_CHANGED = 'VOLUME_CHANGED'
 STOP_SERVER = 'STOP_SERVER'
 RESTARTING_SERVER = "Client disconnected, restarting server.."
+MESSAGE_LENGHT = 10
 
 class Server:
     host = None
     ip = None
     serverSocket = None
     clientSocket = None
+
 
     def __init__(self, ipAddress):
         """
@@ -33,20 +35,31 @@ class Server:
         self.listenForMessages(clientSocket)
 
     def listenForMessages(self, clientSocket):
+        oldMessage = ''
+        values = [0]
         while True:
-            message = self.readMessage(clientSocket)
+            message = clientSocket.recv(2048).decode()
 
-            if message == STOP_SERVER:
-                print("Message: " + STOP_SERVER)
-                self.restartServer(clientSocket)
-                break
-            elif message != '':
-                print("Message: volume - " + message)
-                self.changeVolume(message)
-                clientSocket.send(bytes(VOLUME_CHANGED, "utf8"))
+            if message != oldMessage:
+                print(message)
+                oldMessage = message
 
-    def readMessage(self, clientSocket):
-        return str(clientSocket.recv(1024), "utf8")
+                if message == STOP_SERVER:
+                    print("Message: " + STOP_SERVER)
+                    self.restartServer(clientSocket)
+                    break
+                elif message != '':
+                    command, value = message.split(":", 1)
+                    values[0] = value
+
+                    try:
+                        value = int(value)
+                    except:
+                        pass
+
+                    if command == "volume" and isinstance(value, int):
+                        print(values[0])
+                        self.changeVolume(values[0])
 
     def changeVolume(self, message):
         newVolume = (int(message) / 100 * MAX_VOLUME)
@@ -57,3 +70,57 @@ class Server:
         print(RESTARTING_SERVER)
         clientSocket.close()
         self.startServer()
+
+
+# class Server:
+#     host = None
+#     ip = None
+#     serverSocket = None
+#     clientSocket = None
+#
+#     def __init__(self, ipAddress):
+#         """
+#         Opens a socket for the specified IP address that listens for volume change messages from the client;
+#         """
+#         self.ip = str(ipAddress)
+#         self.startServer()
+#
+#     def startServer(self):
+#         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+#         self.serverSocket.bind((self.ip, PORT))
+#         self.serverSocket.listen(5)
+#
+#         print("Server started on " + self.ip + ":" + str(PORT) + " and listening")
+#         clientSocket, clientAddress = self.serverSocket.accept()
+#
+#         print('Got connection from', clientAddress)
+#         self.listenForMessages(clientSocket)
+#
+#     def listenForMessages(self, clientSocket):
+#         while True:
+#             message = self.readMessage(clientSocket)
+#
+#             if message == STOP_SERVER:
+#                 print("Message: " + STOP_SERVER)
+#                 self.restartServer(clientSocket)
+#                 break
+#
+#             elif message != '':
+#                 print(message)
+#                 command, value = message.split(":", 1)
+#                 if command == "volume":
+#                     self.changeVolume(value)
+#                     clientSocket.send(bytes(VOLUME_CHANGED, "utf8"))
+#
+#     def readMessage(self, clientSocket):
+#         return str(clientSocket.recv(24), "utf8")
+#
+#     def changeVolume(self, message):
+#         newVolume = (int(message) / 100 * MAX_VOLUME)
+#         if newVolume / MAX_VOLUME < 1:
+#             subprocess.call("nircmd.exe setvolume 0 " + str(newVolume) + " " + str(newVolume))
+#
+#     def restartServer(self, clientSocket):
+#         print(RESTARTING_SERVER)
+#         clientSocket.close()
+#         self.startServer()
